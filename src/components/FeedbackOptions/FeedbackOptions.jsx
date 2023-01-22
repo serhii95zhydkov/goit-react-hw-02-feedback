b52cee3cc473145665ef2838dd194a0901ed6178
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-22 11:52:22 +0200

    Create FeedbackOptions.jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,17 @@
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul>
+      {options.map(option => {
+        return (
+          <li key={option}>
+            <button type="button" onClick={() => onLeaveFeedback(option)}>
+              {option}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FeedbackOptions;
